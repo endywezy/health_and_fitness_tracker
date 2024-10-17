216185f15f2830b97b9d7c408b0680e63d0d3de4
endywezy <endywezy@gmail.com>
2024-10-17 05:22:25 +0100

    Add authentication routes for user signup and login

---
+++ health_tracker_backend/routes/auth.js
@@ -0,0 +1,36 @@
+#!/usr/bin/node
+
+const express = require('express');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+const router = express.Router();
+
+// User Signup
+router.post('/signup', async (req, res) => {
+  const { username, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const newUser = new User({ username, password: hashedPassword });
+  
+  try {
+    await newUser.save();
+    res.status(201).json({ message: 'User created' });
+  } catch (err) {
+    res.status(400).json({ error: 'User already exists' });
+  }
+});
+
+// User Login
+router.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+
+  if (!user || !(await bcrypt.compare(password, user.password))) {
+    return res.status(400).json({ error: 'Invalid credentials' });
+  }
+
+  const token = jwt.sign({ id: user._id }, 'secretKey', { expiresIn: '1h' });
+  res.json({ token });
+});
+
+module.exports = router;
