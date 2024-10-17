c7848f9e35964fddf86c14def12061e472d35f69
endywezy <endywezy@gmail.com>
2024-10-17 05:20:59 +0100

    Add route to set user goals

---
+++ health_tracker_backend/routes/goals.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const express = require('express');
+const Goal = require('../models/goal');
+const router = express.Router();
+
+// Set Goal
+router.post('/set', async (req, res) => {
+  const { userId, weeklyExercisesGoal } = req.body;
+  const newGoal = new Goal({ userId, weeklyExercisesGoal });
+  
+  try {
+    await newGoal.save();
+    res.status(201).json({ message: 'Goal set' });
+  } catch (err) {
+    res.status(400).json({ error: 'Error setting goal' });
+  }
+});
+
+module.exports = router;
