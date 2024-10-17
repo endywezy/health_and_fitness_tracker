2a642f096b68c34c6083f21d513cc00fe7f292cd
endywezy <endywezy@gmail.com>
2024-10-17 05:20:03 +0100

    Add initial backend server setup

---
+++ health_tracker_backend/index.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/auth');
+const exerciseRoutes = require('./routes/exercises');
+const goalRoutes = require('./routes/goals');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost/fitnesstracker', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+// Routes
+app.use('/api/auth', authRoutes);
+app.use('/api/exercises', exerciseRoutes);
+app.use('/api/goals', goalRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
