9017f845cdffd1aea48c55a6f21879479056b563
endywezy <endywezy@gmail.com>
2024-10-17 16:54:29 +0100

    Refactor MongoDB connection in index.js

--- index.js
+++ index.js
@@ -11,11 +11,6 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-// Connect to MongoDB
-mongoose.connect('mongodb://localhost/fitnesstracker', { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected'))
-  .catch((err) => console.log(err));
-
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/exercises', exerciseRoutes);
