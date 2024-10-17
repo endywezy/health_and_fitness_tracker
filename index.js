fc9a6fcb3d9868ed6de7a9d2f2162c0d9a3df316
endywezy <endywezy@gmail.com>
2024-10-17 17:19:52 +0100

    Refactor MongoDB connection in index.js

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-#!/usr/bin/node
-
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/auth');
@@ -7,15 +5,27 @@ const exerciseRoutes = require('./routes/exercises');
 const goalRoutes = require('./routes/goals');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 app.use(express.json());
 
-// Routes
-app.use('/api/auth', authRoutes);
-app.use('/api/exercises', exerciseRoutes);
-app.use('/api/goals', goalRoutes);
+// Register Routes
+app.use('/auth', authRoutes); // User authentication routes
+app.use('/exercises', exerciseRoutes); // Exercise management routes
+app.use('/goals', goalRoutes); // Goal management routes
 
+app.get('/', (req, res) => {
+  res.send('Welcome to the Health and Fitness Tracker API!');
+});
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/yourDB', {
+  // MongoDB connection options (no deprecated options needed for newer Mongoose versions)
+}).then(() => {
+  console.log('MongoDB connected');
+}).catch(err => console.log(err));
+
+// Start server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
